5d26939135190efd813a24b73223cc8379e394e4
Brian Michel <bmichel@urbn.com>
2013-04-19 17:42:04 -0400

    Initial Commit

---
+++ NavJunkTests/NavJunkTests.h
@@ -0,0 +1,13 @@
+//
+//  NavJunkTests.h
+//  NavJunkTests
+//
+//  Created by Brian Michel on 4/19/13.
+//  Copyright (c) 2013 Brian Michel. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NavJunkTests : SenTestCase
+
+@end
---
+++ NavJunkTests/NavJunkTests.m
@@ -0,0 +1,32 @@
+//
+//  NavJunkTests.m
+//  NavJunkTests
+//
+//  Created by Brian Michel on 4/19/13.
+//  Copyright (c) 2013 Brian Michel. All rights reserved.
+//
+
+#import "NavJunkTests.h"
+
+@implementation NavJunkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NavJunkTests");
+}
+
+@end
